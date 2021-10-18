ca87ee60a1b012d7511edb3a01f286733ba33a16
glindy <glindy@udel.edu>
2021-10-18 19:21:43 -0400

    main chatlog, can add messages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { sendChat } from './utilities/chat';
+import { sendChat2 } from './utilities/chat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Up to Date
-        </a>
-      </header>
-    </div>
+    <Container className="App" fluid>
+      <Row className="main-page">
+        <Col className="main-page">
+          <Row className="App-header">
+            <h6>Channels</h6>
+          </Row>
+          <Row className="App-body">
+            <p className='chatlog-body'>
+              <h6>Messages</h6>
+              <table id='chat-table' class="table table-striped">
+                <tbody>
+                </tbody>
+              </table>
+            </p>
+          </Row>
+          <Row className="App-chatbox">
+            <label>
+              <input type="text" name="chat-input" id="chat-input" />
+              <Button type="submit" id="chat-submit" onClick={sendChat2}>Send</Button>
+            </label>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/utilities/chat.js
+++ src/utilities/chat.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../index.css';
 import App from '../App';
+import { addValue } from './database';
 
+let current = 0;
+
+export function sendChat() {
+    let msg = document.getElementById("chat-input").value;
+    //let msgRef = {"msg": msg, "num": current};
+    //rtdb.push(chatsRef, msgRef);
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
+    let td = document.createElement('td');
+    //tr.appendChild(document.createTextNode(msg));
+    document.querySelector('tbody').appendChild(tr);
+    tr.appendChild(th);
+    tr.appendChild(td);
+    current++;
+}
+
+export function sendChat2() {
+    let table = document.getElementById("chat-table");
+    let tr = table.insertRow(current);
+    let th = tr.insertCell(0);
+    let td = tr.insertCell(1);
+    td.innerHTML = document.getElementById("chat-input").value;
+    th.innerHTML = current;
+    tr.id = "chat-" + current;
+    th.scope = "row";
+    current++;
+}
