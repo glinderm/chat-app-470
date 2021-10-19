0d7ff08d8b2fce3af94eb843f840acdf11c6817e
glindy <glindy@udel.edu>
2021-10-18 21:10:46 -0400

    chatlog formatted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { sendChat } from './utilities/chat';
-import { sendChat2 } from './utilities/chat';
 
 function App() {
   return (
@@ -18,7 +16,7 @@ function App() {
           <Row className="App-body">
             <p className='chatlog-body'>
               <h6>Messages</h6>
-              <table id='chat-table' class="table table-striped">
+              <table id='chat-table' class="table table-striped table-responsive">
                 <tbody>
                 </tbody>
               </table>
@@ -27,7 +25,7 @@ function App() {
           <Row className="App-chatbox">
             <label>
               <input type="text" name="chat-input" id="chat-input" />
-              <Button type="submit" id="chat-submit" onClick={sendChat2}>Send</Button>
+              <Button type="submit" id="chat-submit" onClick={sendChat}>Send</Button>
             </label>
           </Row>
         </Col>
--- src/utilities/chat.js
+++ src/utilities/chat.js
@@ -5,29 +5,38 @@ import App from '../App';
 import { addValue } from './database';
 
 let current = 0;
+//let count = 0;
+/*
+function deleteChat() {
+    //let delChat = document.getElementById('chat-' + idx);
+    //let parent = delChat.parentElement;
+    //parent.removeChild(delChat);
+    //document.getElementById("chat-" + idx).remove();
+    alert("Btn clicked");
+    count--;
+}
+*/
 
 export function sendChat() {
-    let msg = document.getElementById("chat-input").value;
-    //let msgRef = {"msg": msg, "num": current};
-    //rtdb.push(chatsRef, msgRef);
-    let tr = document.createElement('tr');
-    let th = document.createElement('th');
-    let td = document.createElement('td');
-    //tr.appendChild(document.createTextNode(msg));
-    document.querySelector('tbody').appendChild(tr);
-    tr.appendChild(th);
-    tr.appendChild(td);
-    current++;
-}
+    let msg = document.getElementById("chat-input");
+    if (msg.value === "") {
+        return;
+    }
 
-export function sendChat2() {
     let table = document.getElementById("chat-table");
     let tr = table.insertRow(current);
     let th = tr.insertCell(0);
     let td = tr.insertCell(1);
-    td.innerHTML = document.getElementById("chat-input").value;
+    //let x = tr.insertCell(2);
+    
+    //x.innerHTML = '<Button type="submit" className="xBtn" id="x' + current + '" onClick={deleteChat}>' + 'X' + '</Button>'
+    //x.innerHTML = '<button onClick="deleteChat()">X</button>';
+    td.innerHTML = msg.value;
+    td.className = "chat-row";
     th.innerHTML = current;
     tr.id = "chat-" + current;
     th.scope = "row";
+    th.className = "msg-index";
+    msg.value = "";
     current++;
 }
